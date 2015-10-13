5cee640608c77aadcd55fc95957a2b5d4ed6cd2f
kaminoura koki <amino.t02y@gmail.com>
2015-10-14 00:56:46 +0900

    prototype_photosテーブルを生成

--- protospace/db/migrate/20151013154857_create_prototype_photos.rb
+++ protospace/db/migrate/20151013154857_create_prototype_photos.rb
@@ -1,7 +1,8 @@
 class CreatePrototypePhotos < ActiveRecord::Migration
   def change
     create_table :prototype_photos do |t|
-
+      t.integer  :prototype_id
+      t.string   :status
       t.timestamps null: false
     end
   end
--- protospace/db/schema.rb
+++ protospace/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013153629) do
+ActiveRecord::Schema.define(version: 20151013154857) do
+
+  create_table "prototype_photos", force: :cascade do |t|
+    t.integer  "prototype_id", limit: 4
+    t.string   "status",       limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",      limit: 255
