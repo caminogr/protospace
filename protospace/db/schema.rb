62c4ae8c36dbbba2e5654d420baefc572e6f755b
kaminoura koki <amino.t02y@gmail.com>
2015-10-14 01:10:36 +0900

    prototype_photosテーブルにimageカラムを加えてマイグレーションファイルを実行した

--- protospace/db/schema.rb
+++ protospace/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013154857) do
+ActiveRecord::Schema.define(version: 20151013160823) do
 
   create_table "prototype_photos", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
     t.string   "status",       limit: 255
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "image",        limit: 255
   end
 
   create_table "prototypes", force: :cascade do |t|
