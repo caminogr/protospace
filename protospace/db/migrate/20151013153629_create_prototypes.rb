5ce7443870941d014761f8a19ebd7abe6e8819e2
kaminoura koki <amino.t02y@gmail.com>
2015-10-14 00:46:05 +0900

    prototypesテーブルを生成

--- protospace/db/migrate/20151013153629_create_prototypes.rb
+++ protospace/db/migrate/20151013153629_create_prototypes.rb
@@ -1,7 +1,9 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
-
+      t.string      :title
+      t.text        :catchcopy
+      t.text        :concept
       t.timestamps null: false
     end
   end
