509181d4c6498cbb2b1c2f71bedb15c0d891981e
kaminoura koki <amino.t02y@gmail.com>
2015-10-18 03:18:18 +0900

    imageカラムが空の場合、テーブルに保存されないようにした

--- protospace/app/models/prototype.rb
+++ protospace/app/models/prototype.rb
@@ -1,4 +1,4 @@
 class Prototype < ActiveRecord::Base
   has_many :prototype_photos
-  accepts_nested_attributes_for :prototype_photos
+  accepts_nested_attributes_for :prototype_photos, reject_if: proc{ |attributes| attributes['image'].blank?}
 end
