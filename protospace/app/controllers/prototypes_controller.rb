e280fa88aa99e4a695a42ee27604ed5e44258248
kaminoura koki <amino.t02y@gmail.com>
2015-10-17 11:03:51 +0900

    createアクションをストロングパラメーターを利用して設定し、フォームに投稿した要素を保存できるようにした

--- protospace/app/controllers/prototypes_controller.rb
+++ protospace/app/controllers/prototypes_controller.rb
@@ -5,4 +5,13 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
     @prototype.prototype_photos.build
   end
+
+  def create
+    Prototype.create(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:prototype).permit(:title, :catchcopy, :concept, prototype_photos_attributes: [:prototype_id, :status, :image])
+  end
 end
