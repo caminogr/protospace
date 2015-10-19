a367d6486ec31fc55c3b5561b117b491d9db475e
kaminoura koki <amino.t02y@gmail.com>
2015-10-19 22:34:29 +0900

    マイグレーションファイルを作成・実行し、usersテーブルにworksカラムを追加した

--- protospace/db/schema.rb
+++ protospace/db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema.define(version: 20151013160823) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151019133210) do
 
   create_table "prototype_photos", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -15,6 +28,7 @@ ActiveRecord::Schema.define(version: 20151013160823) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
     t.string   "encrypted_password",     limit: 255,   default: "", null: false
@@ -31,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151013160823) do
     t.string   "username",               limit: 255
     t.string   "member",                 limit: 255
     t.text     "profile",                limit: 65535
+    t.string   "works",                  limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
