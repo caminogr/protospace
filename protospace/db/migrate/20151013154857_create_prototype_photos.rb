bf3cdb50c1ce15bde458ce353c604ffce5232c41
kaminoura koki <amino.t02y@gmail.com>
2015-10-21 13:51:13 +0900

    競合の解決

--- protospace/db/migrate/20151013154857_create_prototype_photos.rb
+++
@@ -1,9 +0,0 @@
-class CreatePrototypePhotos < ActiveRecord::Migration
-  def change
-    create_table :prototype_photos do |t|
-      t.integer  :prototype_id
-      t.string   :status
-      t.timestamps null: false
-    end
-  end
-end
