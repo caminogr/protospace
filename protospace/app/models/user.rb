6b846a2dec348e1bd8f01d93bef11f7a4829f4e0
kaminoura koki <amino.t02y@gmail.com>
2015-10-22 14:10:22 +0900

    ユーザー登録時のバリデーションにusernameを加えた

--- protospace/app/models/user.rb
+++ protospace/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
+  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
