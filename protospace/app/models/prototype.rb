722b2246c23bf87b4d273bdda3580a0fda79b7bc
kaminoura koki <amino.t02y@gmail.com>
2015-10-26 00:09:12 +0900

    prototype投稿にtitle,catch copy,conceptにバリデーションを加えた

--- protospace/app/models/prototype.rb
+++ protospace/app/models/prototype.rb
@@ -2,6 +2,9 @@ class Prototype < ActiveRecord::Base
   has_many :prototype_photos
   belongs_to :user
   accepts_nested_attributes_for :prototype_photos, reject_if: :no_blank?
+  validates :title, presence: true
+  validates :catchcopy, presence: true
+  validates :concept, presence: true
 
   def no_blank?(attributed)
     attributed['image'].blank?
