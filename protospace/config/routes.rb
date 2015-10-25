e076a766336c6a2dc84494c763b01485f3bfb65d
kaminoura koki <amino.t02y@gmail.com>
2015-10-25 11:54:45 +0900

    showアクションを[　]で囲った

--- protospace/config/routes.rb
+++ protospace/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: [:show]
   root 'top#index'
 
   resources :prototypes, only: [:index, :new, :create, :show]
