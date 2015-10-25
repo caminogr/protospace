6343ecf9a489dc9907b98b2719206750bbe915af
kaminoura koki <amino.t02y@gmail.com>
2015-10-26 00:17:57 +0900

    prototype投稿のバリデーションにprototype_photoを加え、画像が投稿されていないと弾くようにした

--- protospace/app/models/prototype.rb
+++ protospace/app/models/prototype.rb
@@ -3,6 +3,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   accepts_nested_attributes_for :prototype_photos, reject_if: :no_blank?
   validates :title, presence: true
+  validates :prototype_photos, presence: true
   validates :catchcopy, presence: true
   validates :concept, presence: true
 
