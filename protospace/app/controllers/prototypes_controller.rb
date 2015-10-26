0b7904224bc2981bc2e44717f1288c09811171cb
kaminoura koki <amino.t02y@gmail.com>
2015-10-26 12:35:45 +0900

    exept: :showをexcept: [:show]に修正

--- protospace/app/controllers/prototypes_controller.rb
+++ protospace/app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, exept: :show
+  before_action :authenticate_user!, except: [:show]
   def show
   end
   def new
