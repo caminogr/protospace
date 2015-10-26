8662b8ed8421ff881677c5e4691d91ab88591bfd
kaminoura koki <amino.t02y@gmail.com>
2015-10-26 21:45:22 +0900

    バリデーションの記述をまとめた

--- protospace/app/models/prototype.rb
+++ protospace/app/models/prototype.rb
@@ -3,10 +3,7 @@ class Prototype < ActiveRecord::Base
   has_many :prototype_photos
   belongs_to :user
   accepts_nested_attributes_for :prototype_photos, reject_if: :no_blank?
-  validates :title, presence: true
-  validates :prototype_photos, presence: true
-  validates :catchcopy, presence: true
-  validates :concept, presence: true
+  validates :title, :prototype_photos, :catchcopy, :concept, presence: true
 
   def no_blank?(attributed)
     attributed['image'].blank?
