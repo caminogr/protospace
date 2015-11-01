a2843c2b014d0089462ac7719d603be99eb88ef8
kaminoura koki <amino.t02y@gmail.com>
2015-11-01 23:00:15 +0900

    likesテーブルを作成した

--- protospace/db/schema.rb
+++ protospace/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023021648) do
+ActiveRecord::Schema.define(version: 20151101102804) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prototype_photos", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -31,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151023021648) do
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
     t.string   "encrypted_password",     limit: 255,   default: "", null: false
