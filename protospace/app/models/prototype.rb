e4135e0662a3bed1dcc4c8b746e2d9061fad403d
kaminoura koki <amino.t02y@gmail.com>
2015-11-04 22:51:56 +0900

    prototypeを削除した時にそれに紐付いたlikesテーブルのレコードも削除されるように設定

--- protospace/app/models/prototype.rb
+++ protospace/app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :delete_all
   has_many :prototype_photos, dependent: :delete_all
   accepts_nested_attributes_for :prototype_photos, reject_if: :no_blank?
   validates :title, :prototype_photos, :catchcopy, :concept, presence: true
