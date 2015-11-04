ff516f00174e051b8124c2f3d62bb381e6a32488
kaminoura koki <amino.t02y@gmail.com>
2015-11-04 22:58:25 +0900

    ajaxを使うので必要ないリダイレクトの設定を削除

--- protospace/app/controllers/likes_controller.rb
+++ protospace/app/controllers/likes_controller.rb
@@ -1,11 +1,9 @@
 class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(prototype_id: params[:prototype_id])
-    redirect_to prototype_path(params[:prototype_id])
   end
 
   def destroy
     Like.find(params[:id]).destroy
-    redirect_to prototype_path(params[:prototype_id])
   end
 end
