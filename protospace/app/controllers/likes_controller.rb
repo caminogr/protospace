f768758da272d3501a23fb793fc554876331e2c3
kaminoura koki <amino.t02y@gmail.com>
2015-11-04 21:49:48 +0900

    like機能を実装する時に非同期通信だとわかりづらいので同期通信にし、likeボタンを押したあと同じページにリダイレクト先を設定

--- protospace/app/controllers/likes_controller.rb
+++ protospace/app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(prototype_id: params[:prototype_id])
+    redirect_to prototype_path(params[:prototype_id])
   end
 end
