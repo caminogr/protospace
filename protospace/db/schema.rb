b5e6f531a092f311bbce5b85235f3c66e8183cbc
kaminoura koki <amino.t02y@gmail.com>
2015-11-06 03:04:39 +0900

    rake db:migrateでマイグレーションファイルを実行

--- protospace/db/schema.rb
+++ protospace/db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023021648) do
+ActiveRecord::Schema.define(version: 20151105175852) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",         limit: 65535
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prototype_photos", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -22,15 +37,17 @@ ActiveRecord::Schema.define(version: 20151023021648) do
   end
 
   create_table "prototypes", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "catchcopy",  limit: 65535
-    t.text     "concept",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "user_id",    limit: 4
+    t.string   "title",       limit: 255
+    t.text     "catchcopy",   limit: 65535
+    t.text     "concept",     limit: 65535
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.integer  "user_id",     limit: 4
+    t.integer  "likes_count", limit: 4,     default: 0
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
     t.string   "encrypted_password",     limit: 255,   default: "", null: false
