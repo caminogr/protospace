f0e49db273ea315270a4135d38f1fc58eb012b96
kaminoura koki <amino.t02y@gmail.com>
2015-11-10 10:52:29 +0900

    投稿したコメントが保存できるようにcommentsコントローラーのcreateアクションを設定

--- protospace/app/controllers/comments_controller.rb
+++ protospace/app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    current_user.comments.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(prototype_id: params[:prototype_id])
   end
 end
