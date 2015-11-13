58746b8d91361a9ccc1495fb62b46edd583efcd5
kaminoura koki <amino.t02y@gmail.com>
2015-11-13 19:06:28 +0900

    prototypeモデルにtagsタグを設置した

--- protospace/app/models/prototype.rb
+++ protospace/app/models/prototype.rb
@@ -3,6 +3,7 @@ class Prototype < ActiveRecord::Base
   has_many :prototype_photos, dependent: :delete_all
   belongs_to :user
   accepts_nested_attributes_for :prototype_photos, reject_if: :no_blank?
+  acts_as_taggable
   validates :title, :prototype_photos, :catchcopy, :concept, presence: true
 
   def no_blank?(attributed)
