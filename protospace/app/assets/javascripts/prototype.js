3ba7800cf45634edb167f99f90b87f73306f501c
kaminoura koki <amino.t02y@gmail.com>
2015-11-17 00:39:06 +0900

    fileの読み込みが成功した時にイベントを発生させる記述をした

--- protospace/app/assets/javascripts/prototype.js
+++ protospace/app/assets/javascripts/prototype.js
@@ -2,6 +2,8 @@ $(window).on("load", function(){
   $('input[type=file]').change(function(){
     var file = $(this).prop('files')[0];
     var reader = new FileReader();
+    reader.onload = function(){
+    }
   });
 });
 
