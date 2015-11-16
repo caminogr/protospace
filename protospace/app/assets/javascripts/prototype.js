ec4655fe622b798f351358379507ae972b7859c3
kaminoura koki <amino.t02y@gmail.com>
2015-11-17 00:45:15 +0900

    選択したfile形式のフォームが何番目のものか取得し、idの指定に加えるための記述をした

--- protospace/app/assets/javascripts/prototype.js
+++ protospace/app/assets/javascripts/prototype.js
@@ -1,9 +1,10 @@
 $(window).on("load", function(){
   $('input[type=file]').change(function(){
+    var i = $('input[type=file]').index(this);
     var file = $(this).prop('files')[0];
     var reader = new FileReader();
     reader.onload = function(){
-      $("#image_preview").attr('src', reader.result).css('display', 'inline');
+      $("#image_preview" + i).attr('src', reader.result).css('display', 'inline');
     }
     reader.readAsDataURL(file);
   });
