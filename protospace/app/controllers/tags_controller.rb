4b1812c555b8c0c35fb56cb0cbf982cd0dba553d
kaminoura koki <amino.t02y@gmail.com>
2015-11-17 23:12:39 +0900

    tagsコントローラーのindexアクションを生成

--- protospace/app/controllers/tags_controller.rb
+++ protospace/app/controllers/tags_controller.rb
@@ -1,4 +1,8 @@
 class TagsController < ApplicationController
+  def index
+    
+  end
+
   def show
     taggings = ActsAsTaggableOn::Tagging.where(tag_id: params[:id])
     @prototypes = taggings.map{|tagging| tagging.taggable}
