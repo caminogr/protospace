5db4e76e52569658d3fc3ed33c36036b20320a26
kaminoura koki <amino.t02y@gmail.com>
2015-11-17 22:56:36 +0900

    taggingsに代入されているtaggingモデルのインスタンスの配列から紐付いたprototypeを取得した

--- protospace/app/controllers/tags_controller.rb
+++ protospace/app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   def show
     taggings = ActsAsTaggableOn::Tagging.where(tag_id: params[:id])
+    @prototypes = taggings.map{|tagging| tagging.taggable}
   end
 end
