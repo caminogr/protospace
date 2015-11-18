d1b9e0656c2fd99ed934c57dda3bb91ff83a4e4c
kaminoura koki <amino.t02y@gmail.com>
2015-11-18 18:46:31 +0900

    1ページに8個の投稿を表示するための記述を加えた

--- protospace/app/controllers/top_controller.rb
+++ protospace/app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
   def index
-    @prototypes = Prototype.order("id ASC")
+    @prototypes = Prototype.order("id ASC").page(params[:page]).per(8)
   end
 end
